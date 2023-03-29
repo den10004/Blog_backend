6e95373e76989297efb0bb1d55d08c009fdf5f52
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-03-29 13:29:38 +0300

    сделаны перменные

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from "cors";
+
 import {
   loginValidation,
   registerValidation,
@@ -40,6 +41,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 app.use(express.json());
+
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
